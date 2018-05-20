9e7ccd0888f3b95c9713dea95087c881fe006e90
Alexandri Anastasia Evgenia <alexandri.a.eugenia@gmail.com>
2018-05-20 17:02:12 +0300

    feat: Cat Clicker Premium Pro
    Add admin view mode. Doesn't work properly yet.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /*----Application Data----*/
 const data = {
     currentCat: null,
+    adminStatus: false,
     cats: [{
             name: "asher",
             url: "images/asher.jpg",
@@ -40,7 +41,7 @@ const octapus = {
         data.currentCat = data.cats[0];
         catView.init();
         catListView.init();
-
+        catAdminView.init();
     },
 
     getCats: function() {
@@ -58,16 +59,41 @@ const octapus = {
     incrementClicks: function() {
         data.currentCat.clicks++;
         catView.render();
+    },
+
+    openCatAdminView: function() {
+        data.adminStatus = true;
+        catAdminView.render();
+    },
+
+    closeCatAdminView: function() {
+        data.adminStatus = false;
+        catAdminView.render();
+    },
+
+    updateCurrentCat: function() {
+        data.currentCat.name = catAdminView.inputName.value;
+        data.currentCat.url = catAdminView.inputUrl.value;
+        data.currentCat.clicks = catAdminView.inputClicks.value;
+
+        catView.render();
+        // catListView.render();
+
+        data.adminStatus = false;
+        catAdminView.render();
+    },
+
+    adminModeStatus: function() {
+        return data.adminStatus;
     }
 
-}
-
+};
 
 
 /*----Application View----*/
 const catListView = {
     init: function() {
-        this.button = $('button');
+        this.button = $('.drop_down_button');
         this.catList = $('.drop_down_contents');
 
         this.render();
@@ -95,6 +121,7 @@ const catListView = {
     }
 };
 
+
 const catView = {
     init: function() {
         this.imgContainer = $('.img_container');
@@ -116,6 +143,48 @@ const catView = {
         this.catImage.attr("src", currentCat.url);
         this.catClicks.text(`Clicks: ${currentCat.clicks}`);
     }
-}
+};
+
+
+const catAdminView = {
+    init: function() {
+        this.adminForm = $('.form');
+        this.inputName = $('.input_name');
+        this.inputUrl = $('.input_url');
+        this.inputClicks = $('.input_clicks');
+
+        const adminButton = $('.admin_button');
+        const submitButton = $('.submit_button');
+        const cancelButton = $('.cancel_button');
+
+        adminButton.click(function() {
+            octapus.openCatAdminView();
+        });
+
+        submitButton.click(function() {
+            octapus.updateCurrentCat();
+        });
+
+        cancelButton.click(function() {
+            octapus.closeCatAdminView();
+        });
+      
+    },
+
+    render: function() {
+        if(octapus.adminModeStatus() === false) {
+            this.adminForm.toggleClass("admin_form");
+        } else {            
+            let newCat = octapus.getCurrentCat();
+
+            this.inputName.value = newCat.name;
+            this.inputUrl.value = newCat.url;
+            this.inputClicks.value = newCat.clicks;
+
+            this.adminForm.toggleClass("admin_form");
+        }
+    }
+};
+
 
 octapus.init();
